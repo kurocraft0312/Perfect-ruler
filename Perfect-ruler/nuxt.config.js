aad8959ac240fe956804318b113dcb185bb34717
kurocraft0312 <kurocraft0312@gmail.com>
2019-12-12 21:24:40 +0900

    metaタグを実装しました。

--- Perfect-ruler/nuxt.config.js
+++ Perfect-ruler/nuxt.config.js
@@ -5,11 +5,24 @@ export default {
   ** Headers of the page
   */
   head: {
+    htmlAttrs: {
+      prefix: 'og: https://ogp.me/ns#'
+    },
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'サイト名' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: '/' },
+      { hid: 'og:title', property: 'og:title', content: 'Perfect-ruler' },
+      { hid: 'og:description', property: 'og:description', content: '文章制作用のテンプレートです。'},
+      { hid: 'og:image', property: 'og:image', content: '.jpg' },
+      { property: 'article:publisher', content: 'FBのURL' },
+      { property: 'fb:app_id', content: 'FBのAppID' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@kurocraft7522' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
