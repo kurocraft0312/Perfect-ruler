db71ada1e109a44b58deba7e45deced209181b77
kurocraft0312 <kurocraft0312@gmail.com>
2019-12-13 19:56:30 +0900

    書き換え

--- Perfect-ruler/layouts/default.vue
+++ Perfect-ruler/layouts/default.vue
@@ -52,3 +52,15 @@ html {
   background-color: #35495e;
 }
 </style>
+<script>
+import Header from '~/components/Header.vue'
+import Main from '~/components/Main.vue'
+import Footer from '~/components/Footer.vue'
+export default {
+  components: {
+    Header,
+    Main,
+    Footer
+  },
+}
+</script>
